235bcc4418a042f9444ee9235e8b46c4ebeadb8a
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-03-18 18:14:55 +0000

    .

--- src/aoc2018/day15.clj
+++ src/aoc2018/day15.clj
@@ -1,15 +1,19 @@
 (ns day15
   (:require [clojure.test :refer [deftest is testing]]
             [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [taoensso.tufte :as tufte :refer [p profiled profile]]))
 
 ;; helpers and debug
 
-(defn tap>> [note x]
+(defn- read-order [[x y]] [y x])
+
+
+(defn- tap>> [note x]
   (tap> [note x])
   x)
 
-(defn grid->string
+(defn- grid->string
   "Takes a map of [x y]->val, and a function which transforms values to strings, and returns
    a string representation of the grid, with 0,0 being the top left"
   [f m]
@@ -22,17 +26,18 @@
                            x (range (apply min xs) (inc (apply max xs)))]
                        (apply str (f (m [x y])))))))))
 
-(defn print-grid [m]
+(defn- print-grid [m]
   (println (grid->string
             (fn [[typ _]]
               (if (nil? typ) "#" (typ {:elf "E" :goblin "G" :empty "."})))
             m))
-  (doall (map println (filter (fn [[k [typ hp]]] (#{:goblin :elf} typ)) m))))
+  (doall (map println (sort-by (comp read-order first) (filter (fn [[k [typ hp]]] (#{:goblin :elf} typ)) m))))
+  (println "DONE!"))
 
 
 ;; parsing
 
-(defn parse-input [string]
+(defn- parse-input [string]
   (into {} (apply concat
                   (map-indexed
                    (fn parse-row [y row]
@@ -203,7 +208,6 @@
  state: coord -> unit | [:empty]
  "
 
-(defn- read-order [[x y]] [y x])
 
 ;; Level 1: Coordinates
 
@@ -245,12 +249,12 @@
 
 (def enemy-of {:goblin :elf :elf :goblin})
 
-(defn spaces-containing-full
+(defn- units
   "Returns the spaces containing the elements of the set 'contents'"
   [state contents]
   (sort-by read-order (keep (fn [[coord [type hp]]] (when (contents type) [coord [type hp]])) state)))
 
-(def spaces-containing (comp #(sort-by read-order %) #(map first %) spaces-containing-full))
+(def spaces-containing (comp #(sort-by read-order %) #(map first %) units))
 
 (defn- turn-order
   "Given the state, returns a sequence of coordinates with units in them, in the order in which they
@@ -278,20 +282,20 @@
 
 ;; level 3 moving
 
-(defn move [state current-coord]
+(defn- move [state current-coord]
   (if-let [[_ move-to] (path-to-closest-attack-square state current-coord)]
     (-> state
         (assoc move-to (state current-coord))
         (assoc current-coord [:empty]))
     state))
 
-(defn pick-target [state coord]
-  (->> (spaces-containing-full state #{(enemy-of (first (state coord)))})
+(defn- pick-target [state coord]
+  (->> (units state #{(enemy-of (first (state coord)))})
        (filter #((neigbours coord) (first %)))
        (sort-by (fn [[coord [_ hp]]] [hp (read-order coord)]))
        (ffirst)))
 
-(defn attack [state target]
+(defn- attack [state target]
   (if target
     (let [[_ hp] (state target)]
       (if (> hp 3)
@@ -299,7 +303,7 @@
         (assoc state target [:empty])))
     state))
 
-(defn move-and-attack [state current-coord]
+(defn- move-and-attack [state current-coord]
   (if-let [[_ move-to] (path-to-closest-attack-square state current-coord)]
     (let [new-state (-> state
                         (assoc move-to (state current-coord))
@@ -307,114 +311,42 @@
       (attack new-state (pick-target new-state move-to)))
     state))
 
-
-(comment
-  (attack {[1 1] [:goblin 3]} [1 1]))
-
-(defn turn [state coord]
+(defn- turn [state coord]
   (if (= [:empty] (state coord))
     state
     (if-let [target (pick-target state coord)]
       (attack state target)
       (move-and-attack state coord))))
 
-(defn round [state]
+(defn- round [state]
   (reduce turn state (turn-order state)))
-
-(comment
-  (= example5-1 (round example5-0)))
 
 (defn play [state it]
   (cond (> it 1000) :break
         (or (empty? (spaces-containing state #{:goblin}))
             (empty? (spaces-containing state #{:elf})))
-        [it (into {} (spaces-containing-full state #{:goblin :elf})) (* (dec it) (apply + (map (comp second second) (spaces-containing-full state #{:goblin :elf}))))]
+        [it (into {} (units state #{:goblin :elf})) (* (dec it) (apply + (map (comp second second) (units state #{:goblin :elf}))))]
         :else (recur (round state) (inc it))))
 
-(comment
-  (is (= [47 {[5 3] [:goblin 59], [2 2] [:goblin 131], [1 1] [:goblin 200], [5 5] [:goblin 200]} 27730]
-         (play example5-0 0)))
-
-  ((last (take 28 (iterate round example5-0))) [5 4])
-  ((last (take 29 (iterate round example5-0))) [5 4])
-
-  (= example4-1 (round example4-0))
-  (= (assoc example4-2
-            [4 2] [:goblin 197]
-            [4 3] [:elf 197]) (round example4-1))
-  (print-grid example4-2)
-  (print-grid (-> example4-2 round))
-
-
-  (= example5-2 (round example5-1))
-  (into {} (spaces-containing-full (last (take 49 (iterate round example5-0))) #{:goblin :elf}))
-
-  (print-grid (last (take 49 (iterate round example5-0)))))
-
-(comment
-  (is (= example4-1 (reduce turn example4-0 (turn-order example4-0))))
-  (merge-with vector example4-2 (reduce turn example4-1 (turn-order example4-1)))
-  (print-grid (reduce turn example4-1 (turn-order example4-1)))
-  (print-grid example4-2)
-  (print-grid (reduce turn example4-2 (turn-order example4-2)))
-
-
-  (let [state (reduce turn example4-2 [[3 1] [5 1] [4 2] [2 3] [4 3] [6 3] [1 5] [4 5]])]
-    (turn state [7 5]))
-  '([3 1] [5 1] [4 2] [2 3] [4 3] [6 3] [1 5] [4 5] [7 5]))
-
-(comment
-  (= example4-2 (reduce move example4-1 (turn-order example4-1)))
-  (sort-by (comp read-order first) (merge-with vector example4-2 (reduce move example4-1 (turn-order example4-1))))
-  (sort-by (comp read-order first) example4-1))
-
-(defn test-map
+(defn- test-map
   [state assertions]
   (for [[coord unit] assertions]
     (state coord)))
 
+(play (parse-input "#######\n#.G...#\n#...EG#\n#.#.#G#\n#..G#E#\n#.....#\n#######") 0)
+(print-grid (last (take 48 (iterate round (parse-input "#######\n#.G...#\n#...EG#\n#.#.#G#\n#..G#E#\n#.....#\n#######")))))
+(print-grid (last (take 38 (iterate round (parse-input "#######\n#G..#E#\n#E#E.E#\n#G.##.#\n#...#E#\n#...E.#\n#######")))))
 (comment
-  (test-map example [[[1 2] [:goblin 200]]])
+  (play (parse-input (slurp "resources/day15input")) 0)
 
-  example
-
-  (turn {[4 3] [:empty]
-         [2 2] [:elf 200]
-         [0 0] [:goblin 9]
-         [1 0] [:empty]
-         [2 3] [:goblin 2]
-         [3 3] [:empty]
-         [1 1] [:empty]
-         [3 4] [:goblin 1]
-         [4 2] [:empty]
-         [3 0] [:empty]
-         [4 1] [:empty]
-         [1 4] [:empty]
-         [1 3] [:empty]
-         [0 3] [:empty]
-         [2 4] [:empty]
-         [0 2] [:empty]
-         [2 0] [:empty]
-         [0 4] [:empty]
-         [3 1] [:empty]
-         [2 1] [:goblin 4]
-         [4 4] [:empty]
-         [1 2] [:empty]
-         [3 2] [:goblin 2]
-         [0 1] [:empty]
-         [4 0] [:empty]}
-        [2 2]))
-
-(let [s (parse-input "#######\n#G..#E#\n#E#E.E#\n#G.##.#\n#...#E#\n#...E.#\n#######")]
-  (play s 0))
-
-(let [s (parse-input "#######\n#E..EG#\n#.#G.E#\n#E.##E#\n#G..#.#\n#..E#.#\n#######")]
-  (play s 0))
-
-(comment
-  (play example5-0 0))
+  (time (print-grid (last (take 1 (iterate round (parse-input (slurp "resources/day15input"))))))))
 
 (deftest play-test
+  (is (= 36334 (last (play (parse-input "#######\n#G..#E#\n#E#E.E#\n#G.##.#\n#...#E#\n#...E.#\n#######") 0))))
+  (is (= 39514 (last (play (parse-input "#######\n#E..EG#\n#.#G.E#\n#E.##E#\n#G..#.#\n#..E#.#\n#######") 0))))
+  (is (= 27755 (last (play (parse-input "#######\n#E.G#.#\n#.#G..#\n#G.#.G#\n#G..#.#\n#...E.#\n#######") 0))))
+  (is (= 28944 (last (play (parse-input "#######\n#.E...#\n#.#..G#\n#.###.#\n#E#G#G#\n#...#G#\n#######") 0))))
+  (is (= 18740 (last (play (parse-input "#########\n#G......#\n#.E.#...#\n#..##..G#\n#...##..#\n#...#...#\n#.G...G.#\n#.....G.#\n#########") 0))))
   (is (= [47 {[5 3] [:goblin 59], [2 2] [:goblin 131], [1 1] [:goblin 200], [5 5] [:goblin 200]} 27730]
          (play example5-0 0))))
 
